fd5b601ba41b71d5f53f46b61091087062121204
Hazno21688 <62346467+Hazno-dev@users.noreply.github.com>
2026-01-12 18:45:34 +0000

    fixed cmake msvc compiler defs and changed to gen exprs
    i cannot believe i had it set to INTERFACE ;-;

--- CMake/Platform/MSVC.cmake
+++ CMake/Platform/MSVC.cmake
@@ -7,32 +7,37 @@ set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<CONFIG:Debug>:Debug>DLL")
 set(CMAKE_SYSTEM_NAME Windows)
 
 function(Pro_ApplyPlatform TargetName)
-    set(TARGET_COMPILE_OPTIONS /W0 /Gy /Oi /sdl /GS- /std:c++20 /permissive-)
-    set(TARGET_COMPILE_DEFS _WINDOWS _WINSOCKAPI_ prometheus_EXPORTS NOGDI _USRDLL SPDLOG_FMT_EXTERNAL IMGUI_DEFINE_MATH_OPERATORS IMGUI_USE_WCHAR32)
-    set(TARGET_COMPILE_FEATURES cxx_std_20)
-    set(TARGET_LINK_OPTIONS /DLL /DEBUG)
+    set(TARGET_COMPILE_OPTIONS
+            /W0 /Gy /Oi /GS- /std:c++20 /Zc:preprocessor /permissive-
+            $<$<CONFIG:Release>:/Zi>
+            $<$<CONFIG:Debug>:/EHa- /EHs- /EHc- /EHr->
+    )
 
-    if(${CMAKE_BUILD_TYPE} STREQUAL "Release")
-        set(TARGET_COMPILE_OPTIONS ${TARGET_COMPILE_OPTIONS} /Gy /Zi )
-        set(TARGET_COMPILE_DEFS ${TARGET_COMPILE_DEFS} WIN32_LEAN_AND_MEAN NDEBUG _CRT_SECURE_NO_WARNINGS)
-    endif()
+    set(TARGET_COMPILE_DEFS
+            _WINDOWS _WINSOCKAPI_ prometheus_EXPORTS NOGDI _USRDLL
+            SPDLOG_FMT_EXTERNAL IMGUI_DEFINE_MATH_OPERATORS IMGUI_USE_WCHAR32
+            $<$<CONFIG:Release>:WIN32_LEAN_AND_MEAN NDEBUG _CRT_SECURE_NO_WARNINGS>
+    )
 
-    if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
-        set(TARGET_COMPILE_OPTIONS ${TARGET_COMPILE_OPTIONS} /EHa- /EHs- /EHc- /EHr-)
-        set_target_properties(${TargetName} PROPERTIES WINDOWS_EXPORT_ALL_SYMBOLS ON)
-    endif()
+    set(TARGET_COMPILE_FEATURES
+            cxx_std_20
+    )
+
+    set(TARGET_LINK_OPTIONS
+            /DLL /DEBUG
+    )
 
     message("Configuring target: ${TargetName} with options:")
     message(STATUS "Linker options          : ${TARGET_LINK_OPTIONS}")
     message(STATUS "Compilation options     : ${TARGET_COMPILE_OPTIONS}")
     message(STATUS "Compilation definitions : ${TARGET_COMPILE_DEFS}")
     message(STATUS "Compilation features    : ${TARGET_COMPILE_FEATURES}")
-    message(STATUS "Toolchain: ${CMAKE_TOOLCHAIN_FILE}")
+    message(STATUS "Toolchain               : ${CMAKE_TOOLCHAIN_FILE}")
 
     target_link_options(${TargetName} PRIVATE ${TARGET_LINK_OPTIONS})
-    target_compile_options(${TargetName} INTERFACE ${TARGET_COMPILE_OPTIONS})
+    target_compile_options(${TargetName} PRIVATE ${TARGET_COMPILE_OPTIONS})
     target_compile_definitions(${TargetName} PRIVATE ${TARGET_COMPILE_DEFS})
     target_compile_features(${TargetName} PRIVATE ${TARGET_COMPILE_FEATURES})
-
+    set_target_properties(${TargetName} PROPERTIES WINDOWS_EXPORT_ALL_SYMBOLS ON)
     target_include_directories(${TargetName} PUBLIC ${VCPKG_IMPORT_PREFIX}/include)
 endfunction()
